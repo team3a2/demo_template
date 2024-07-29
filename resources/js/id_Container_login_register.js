ed6c5dda22f49c4a1f7b56da5e1a5056c5f760f9
proj7436 <umbalaavc16@gmail.com>
2024-07-29 10:01:46 +0700

    donw reponsive

--- resources/js/id_Container_login_register.js
+++ resources/js/id_Container_login_register.js
@@ -23,7 +23,10 @@ container.querySelector(".close").addEventListener('click', () =>{
     container.classList.add('none')
     document.querySelector('.blackscreen').classList.add('none')
 })
-
+document.querySelector('.blackscreen').addEventListener('click', () =>{
+    container.classList.add('none')
+    document.querySelector('.blackscreen').classList.add('none')
+})
 const account_button = document.querySelector("#header .navbars .accout-js")
 account_button.addEventListener('click', () => {
     document.querySelector('.blackscreen').classList.remove('none')
